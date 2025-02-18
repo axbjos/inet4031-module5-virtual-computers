33b28dd4bd94844a615fb68d35f8892db7fbacff
Joe Axberg <jaxberg@umn.edu>
2025-02-18 15:01:52 -0600

    updates

--- user-processing.py
+++ user-processing.py
@@ -1,6 +1,6 @@
 # INET4031 Module 5
 #
-# User List Processing Program
+# User List Processing Program (simplified)
 #
 # Created: Nov 11, 2024
 # Updated: Feb 10, 2025
@@ -8,31 +8,14 @@
 
 def main():
 
-    #open the user list file
-    try:
 
-        userFile = open("user-list.txt", "r")
-
-    except:
-        print("\nCould not read the file specified.\n")
-        exit()
+    userFile = open("user-list.txt", "r")
 
     #load the lines of the file into a list
     listOfUsers = userFile.readlines()
-    print("\nlist-of-users was read.")
+  
+    for userline in listOfUsers:
 
-    #ask the user to proceed
-    answer = input("\nDo you want to print out the list of users? (Y or N)")
-
-    if answer == "Y" or answer == "y":
-
-        for userline in listOfUsers:
-            print("\n", userline)
-
-    else:
-
-        print("\nOk not printing, ending program.")
-
-    print("\nEnd of User Processing\n")
+        print(userline)
 
 main()
--- user-processing2.py
+++ user-processing2.py
@@ -3,22 +3,10 @@
 import sys
 
 def main():
-  
-    #ask the user to proceed
-    answer = input("\nDo you want to print out the list of users? (Y or N)")
 
-    if answer == "Y" or answer == "y":
+    for line in sys.stdin:
 
-        for line in sys.stdin:
-
-            print(line)
-
-    else:
-
-        print("\nOk not printing, ending program.")
-
-    print("\nEnd of User Processing\n")    
-
+        print(line)
 
 if __name__ == '__main__':
     main()

4c60c6d1077c2e8aa87cf03888b00c97b7fea562
Joe Axberg <jaxberg@umn.edu>
2025-02-18 14:59:09 -0600

    updates

--- user-processing2.py
+++ user-processing2.py
@@ -1,6 +1,24 @@
+#!/usr/bin/python3
+
+import sys
+
 def main():
   
-    for line in sys.stdin:
+    #ask the user to proceed
+    answer = input("\nDo you want to print out the list of users? (Y or N)")
+
+    if answer == "Y" or answer == "y":
+
+        for line in sys.stdin:
+
+            print(line)
+
+    else:
+
+        print("\nOk not printing, ending program.")
+
+    print("\nEnd of User Processing\n")    
+
 
 if __name__ == '__main__':
     main()

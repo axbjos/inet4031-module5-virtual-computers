0fbad7e9948a0f48540f96c950ce1029e7d1bb3c
Joe Axberg <joeaxberg@axnetlabs.org>
2025-02-18 14:49:29 -0600

    Update user-processing.py

--- user-processing.py
+++ user-processing.py
@@ -11,7 +11,7 @@ def main():
     #open the user list file
     try:
 
-        userFile = open("list-of-sers.txt", "r")
+        userFile = open("user-list.txt", "r")
 
     except:
         print("\nCould not read the file specified.\n")
